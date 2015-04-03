227d98d0d03766ff4d5c0224e5baf4c19334ad74
Michael Cheng <cheng.c.mike@gmail.com>
2015-04-03 18:58:39 -0500

    improve bullettime functionality

--- watchout.js
+++ watchout.js
@@ -8,7 +8,7 @@
                 .style('border', '10px solid black');
 
   var enemies = board.selectAll('.enemy')
-                     .data(d3.range(30))
+                     .data(d3.range(10))
                      .enter()
                      // .append('image')
                      // .attr({x: function(){return Math.random() * 700}, y: function(){return Math.random() * 400}, width: '30px', height: '30px', 'xlink:href': 'shuriken.png'})
@@ -17,9 +17,13 @@
                      .attr({cx: function(){return Math.random() * 700}, cy: function(){return Math.random() * 400}, r: 10, d: 'M150 0 L75 200 L225 200 Z'})
                      .style('fill', 'darkred');
 
-  var bulletTime = board.append('circle')
-                        .attr({cx: function(){return Math.random() * 700}, cy: function(){return Math.random() * 400}, r: 10})
-                        .style('fill', 'lightgreen');
+  var createBulletTime = function(){
+    return board.append('circle')
+           .attr({class: 'bullet-time', cx: function(){return Math.random() * 700}, cy: function(){return Math.random() * 400}, r: 10})
+           .style('fill', 'lightgreen');
+  };
+
+  var bulletTime = createBulletTime();
 
   var drag = d3.behavior.drag().on('drag', function(){
     var x = d3.event.x;
@@ -38,9 +42,9 @@
       y = 10;
     }
     if (Math.abs(x - bulletTime.attr('cx')) <= 20 && Math.abs(y - bulletTime.attr('cy')) <= 20) {
-      stepInterval = 3000;
-      // clearInterval(stepper);
-      // stepper = setInterval(randomStep, stepInterval);
+      stepInterval += 1500;
+      board.selectAll('.bullet-time').data([]).exit().remove();
+      bulletTime = createBulletTime();
     }
     player.attr({cx: x, cy: y});
   });
@@ -69,6 +73,7 @@
       
       currentScore = 0;
       d3.select('.current span').text(currentScore);
+      stepInterval = 1500;
     }
   };
 
